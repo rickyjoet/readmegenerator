624f4e914395d7f0990889466475c3bb82a1528b
Ricky <rickyjoet@gmail.com>
2021-07-19 12:24:31 -0700

    added function to write readme

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const path = require('path');
+
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -15,7 +20,9 @@ const questions = [
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
 
 // TODO: Create a function to initialize app
 function init() {}
