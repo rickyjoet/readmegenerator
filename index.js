048bf4bfb832dbd3fb371a0fd12c975a244462e5
Ricky <rickyjoet@gmail.com>
2021-07-19 11:51:27 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    "What is your project title?", 
+    "Describe your project", 
+    "What are installation instructions", 
+    "Describe the usage information", 
+    "What are the contribution guidelines", 
+    "What are the test instructions?",
+    "What license does your project have?",
+    "What is your GitHub username?",
+    "What is your email?"
+];
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
